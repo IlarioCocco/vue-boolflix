8135fa66a430ef4393fd63eced580cba4557fad7
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-07 13:10:48 +0200

    commit axios

--- src/App.vue
+++ src/App.vue
@@ -22,13 +22,13 @@ export default {
   created(){
     axios.get('https://api.themoviedb.org/3/search/movie', {
        params: {
-         api_key: 'e99307154c6dfb0b4750f6603256716d',
-         query: 'ritorno+al+futuro',
-         language: 'it-IT',
+         api_key: '93138e86c1b2f8ca76e512cc0d72943b',
+         query:   'Ritorno al futuro',
+         language:'it-IT',
       }
     })
-    .then(function(risposta) {
-      console.log(risposta.data);
+    .then(function (response) {
+      console.log(response.data);
     });
   }
 }
